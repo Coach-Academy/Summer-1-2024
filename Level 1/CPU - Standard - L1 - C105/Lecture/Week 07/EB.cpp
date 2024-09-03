f38e188beaf3f6b30220e910437a113e91e92a4f
abdualla1 <38115506+abdualla1@users.noreply.github.com>
2024-09-04 00:17:46 +0300

    Update EB.cpp
    added the other example

--- Level 1/CPU - Standard - L1 - C105/Lecture/Week 07/EB.cpp
+++ Level 1/CPU - Standard - L1 - C105/Lecture/Week 07/EB.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-
+// this code outputs correctly
 using namespace std;
 bool isPal(string s){ // q
     int q = (int)s.size();
@@ -23,6 +23,30 @@ int main(){
     if(l != -1 && r != -1){
         string temp = s.substr(l,r-l+1); // q
         cout << (isPal(temp) ? "Yes" : "No");
+    }
+    return 0;
+}
+
+
+////////////////////////////////////////////////////////////////
+
+// this code below DOESNT OUTPUT CORRECTLY  its only example for time complexity
+int main(){
+    int n;
+    cin >> n ;
+    string s; cin >> s;
+    int l = -1,r= -1;
+    for(int i = 0; i < n;i++) {
+        if (s[i] == 'X' && l == -1)
+            l = i;
+        if (s[i] == 'X')
+            r = i;
+        if(l != -1 && r != -1){
+        string temp = s.substr(l,r-l+1); // q
+        cout << (isPal(temp) ? "Yes" : "No");
+l = -1;
+r = -1;
+    }
     }
     return 0;
 }
