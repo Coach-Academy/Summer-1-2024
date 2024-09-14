a1f0024a6444e4cd5aaa42facca00d6241026872
abdualla1 <38115506+abdualla1@users.noreply.github.com>
2024-09-14 20:47:01 +0300

    Update G.cpp

--- Level 1/CPU - Standard - L1 - C105/Practice G1/Week 08/G.cpp
+++ Level 1/CPU - Standard - L1 - C105/Practice G1/Week 08/G.cpp
@@ -1,17 +1,57 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-const int N = 1e5 + 5;
-int arr[N];
 
-int main() {
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-    int cnt = 1;
-    for (int i = 1; i < n; i++)
-        cnt += (arr[i] != arr[i - 1]);
-    cout << cnt << '\n';
+int main(){
+    int q;
+    cin >> q;
+    deque<int> d;
+    bool rev = false;
+    while(q--){
+        string s;
+        cin >> s;
+        if(s == "push_back"){
+            int x; cin >> x;
+            if(!rev)
+                d.push_back(x);
+            else
+                d.push_front(x);
+        }
+        if(s == "toFront"){
+            int x; cin >> x;
+            if(!rev)
+                d.push_front(x);
+            else
+                d.push_back(x);
+        }
+        if(s == "front"){
+            if(d.empty()){
+                cout << "No job for Ada?" << '\n';
+                continue;
+            }
+            if(!rev){
+                cout << d.front() << '\n';
+                d.pop_front();
+            } else {
+                cout << d.back() << '\n';
+                d.pop_back();
+            }
+        }
+        if(s == "back"){
+            if(d.empty()){
+                cout << "No job for Ada?" << '\n';
+                continue;
+            }
+            if(!rev){
+                cout << d.back() << '\n';
+                d.pop_back();
+            } else {
+                cout << d.front() << '\n';
+                d.pop_front();
+            }
+        }
+        if(s == "reverse")
+            rev = !rev;
+    }
     return 0;
 }
